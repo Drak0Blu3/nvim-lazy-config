fea784b2cf2d03f2512db03e3f0981316f427a1a
Drak0Blu3 <luisanchezmolina2@gmail.com>
2024-08-31 14:59:11 -0400

    fix message nvim-hop section

--- lua/plugins/nvim-hop.lua
+++ lua/plugins/nvim-hop.lua
@@ -4,7 +4,7 @@ return {
   event = "VeryLazy",
   opts = {},
   keys = {
-    { "<leader>m", desc = "Easy Motion" },
+    { "<leader>m", "", desc = "Easy Motion" },
     { "<leader>m2", ":HopChar2<CR>", desc = "Easy Motion Char 2" },
     { "<leader>mm", ":HopWord<CR>", desc = "Easy Motion Word" },
     { "<leader>ml", ":HopLine<CR>", desc = "Easy Motion Line" },
