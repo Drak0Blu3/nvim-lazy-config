d03fb17c9ec22b33a15e15e15920b9de52762cf6
Drak0Blu3 <luisanchezmolina2@gmail.com>
2024-09-25 11:02:32 -0400

    comentar codigo q no se usa

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.tabstop = 2 -- Número de espacios que representa un tab
-vim.opt.shiftwidth = 2 -- Tamaño del indentado
-vim.opt.expandtab = true -- Usar espacios en lugar de tabs
-vim.opt.smartindent = true -- Indentado inteligente basado en la sintaxis
-vim.opt.autoindent = true -- Copiar indentado de la línea anterior
+--vim.opt.tabstop = 2 -- Número de espacios que representa un tab
+--vim.opt.shiftwidth = 2 -- Tamaño del indentado
+--vim.opt.expandtab = true -- Usar espacios en lugar de tabs
+--vim.opt.smartindent = true -- Indentado inteligente basado en la sintaxis
+--vim.opt.autoindent = true -- Copiar indentado de la línea anterior
