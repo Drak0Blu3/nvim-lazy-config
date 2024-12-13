cb2e63fba2e32cbabf316807745b91d3502d1f65
Drak0Blu3 <luisanchezmolina2@gmail.com>
2024-12-13 15:11:39 -0400

    change biome config

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -7,9 +7,7 @@ return {
       biome = {
         root_dir = function(fname)
           return util.root_pattern("biome.json", "biome.jsonc")(fname)
-            or util.find_package_json_ancestor(fname)
-            or util.find_node_modules_ancestor(fname)
-            or util.find_git_ancestor(fname)
+            or vim.fs.find("node_modules", { path = fname, upward = true })[1]
         end,
       },
     },
