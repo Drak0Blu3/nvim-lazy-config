6ffaf0d386316af37fe4e8d18ffaf91c9393faba
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-03-12 21:15:09 -0400

    remove external blade parser and use native treesitter blade parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -33,29 +33,4 @@ return {
       "rust",
     })
   end,
-
-  config = function(_, opts)
-    ---@class ParserInfo[]
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = {
-          "src/parser.c",
-          -- 'src/scanner.cc',
-        },
-        branch = "main",
-        generate_requires_npm = true,
-        requires_generate_from_grammar = true,
-      },
-      filetype = "blade",
-    }
-    vim.filetype.add({
-      pattern = {
-        [".*%.blade%.php"] = "blade",
-      },
-    })
-
-    require("nvim-treesitter.configs").setup(opts)
-  end,
 }
