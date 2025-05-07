2201ea4b7425badcea952fded79a71d465ba74ea
Drak0Blu3 <luisanchezmolina2@gmail.com>
2025-05-07 17:06:20 -0400

    temp fix to mason plugin

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,7 @@
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "biome",
@@ -14,7 +15,8 @@ return {
     },
   },
   {
-    "williamboman/mason-lspconfig.nvim",
+    "mason-org/mason-lspconfig.nvim",
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "lua_ls",
